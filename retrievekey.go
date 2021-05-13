d460847cf3a11a83cd4d2f4bfa38ec8ba6b6f5b9
Tim Carolan <41997142+tcarolanthycotic@users.noreply.github.com>
2021-05-13 10:39:19 +0100

    Create retrievekey.go

---
+++ retrievekey.go
@@ -0,0 +1,11 @@
+package retrievekey
+
+type mykey struct {
+	clientid, clientsecret string
+}
+
+func keyreturn(clientid string, clientsecret string) *mykey {
+	key := mykey{clientid: clientid}
+	key.clientsecret = clientsecret
+	return &key
+}
